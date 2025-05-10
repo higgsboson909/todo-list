f6a9a06a62a77afecd0117e307749ba0b6225bd6
higgsboson909 <higgsbosson906@gmail.com>
2025-05-11 02:06:39 +0500

    Able to edit project todos and general todos

--- src/index.js
+++ src/index.js
@@ -36,14 +36,22 @@ const Todo_Item = class {
         }
     }
 
+    editItem (title, description, date, priority, project="inbox") {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.priority = priority;
+        this.project = project;
+    }
+
+    // editItem (index, ti)
+
 }
 
 
-
-
 // create and save the item in data-structure
-const createTodoItem = (title, description, data, priority, project) => {
-    let todoItem = new Todo_Item (title, description, data, priority, project);
+const createTodoItem = (title, description, date, priority, project) => {
+    let todoItem = new Todo_Item (title, description, date, priority, project);
     todoItem.saveTodoItem();
 
 }
@@ -53,6 +61,28 @@ createTodoItem("Ai Project", "do it now", "12-2-2025", "1", "Ai");
 createTodoItem("Python Project", "do it now", "12-2-2025", "1", "Python");
 createTodoItem("General", "do it now", "12-2-2025", "1", "Python");
 createTodoItem("Inbox", "do it now", "12-2-2025", "1", "");
+
+function general2project (title, description, date, priority, project="inbox", index) {
+    if ((index == undefined) && (project == "" || project == undefined || project == "inbox")) {
+        inbox[0].editItem (title, description, date, priority, project)
+    }
+    else {
+        for (let i = 0; i < p.length; i++) {
+            let name = `p${i}`;
+            if (p[i][name] == project && index < p[i].todo.length) {
+                console.log(p[i][name])
+                p[i].todo[index].editItem(title, description, date, priority, project);
+            }
+        }
+    }
+
+}
+
+general2project ("algo-course", "do it before summer", "date", "1", "");
+general2project ("Node js", "do it before summer too", "date", "1");
+general2project ("Machine learn", "Mustufa has done this", "date", "2", "Ai", 0);
+general2project ("Scikit learn", "As an ass", "date", "2", "Python", 1);
+
 
 console.log("Projects -----------", p);
 console.log("Inbox ---------------", inbox);
