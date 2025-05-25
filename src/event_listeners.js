f9bbf3ee8d52aa927188eb2439561161cf85d49c
higgsboson909 <higgsbosson906@gmail.com>
2025-05-25 14:17:05 +0500

    Add eventlisteners to the sidebar

--- src/event_listeners.js
+++ src/event_listeners.js
@@ -1,53 +1,64 @@
-import { inbox, today, upcomingItems } from "./index";
-import { projects } from "./projects";
-import { renderMainHeading, renderTodayItems } from "./render_elements";
-import { defaultSidebarEl, projectsEl } from "./dom_elements";
+// import { inbox, today, upcomingItems } from "./index";
+// import { renderMainHeading, renderTodayItems } from "./render_elements";
+import { defaultSidebarEl, projectSidebarEl, todoItemsListEl } from "./dom_elements";
 
 
 function defaultSidebarEl_evL () {
-    defaultSidebarEl.forEach(el => {
-        el.addEventListener ('click', (e) => {
-            if (e.target.className == "Inbox") {
-                renderTodos(e.target.className)
-                console.log(inbox);
-            }
-            else if (e.target.className == "Today") {
-                renderTodos(e.target.className)
-                renderTodayItems(today)
-                console.log(today);
-            }
-            else if (e.target.className == "Upcoming") {
-                renderTodos(e.target.className)
-                console.log (upcomingItems(inbox));
-                // checking 
-                projects.forEach((project) => {
-                        console.log(upcomingItems(project.todo));
-                });
-            }
-            console.log (`clicked ${e.target.className}`);
-        });
+defaultSidebarEl.forEach(el => {
+    el.addEventListener ('click', (e) => {
+        if (e.target.className == "Inbox") {
+            // renderTodos(e.target.className)
+        }
+        else if (e.target.className == "Today") {
+            // renderTodos(e.target.className)
+            // renderTodayItems(today)
+            // console.log(`clicked ${e.target.className}`);
+        }
+        else if (e.target.className == "Upcoming") {
+            // renderTodos(e.target.className)
+            // console.log (upcomingItems(inbox));
+            // checking 
+            // projects.forEach((project) => {
+            //         console.log(upcomingItems(project.todo));
+            // });
+        }
+        console.log (`clicked ${e.target.className}`);
+    });
 
+});
+}
+
+function projectSidebarEl_evL() {
+    projectSidebarEl.forEach(element => {
+        element.addEventListener('click', (e) => {
+            console.log (`clicked ${e.target.className}`);
+            if (e.target.className) {
+
+                // get specific project
+                // projects.forEach((project) => {
+
+                //     renderTodos(e.target.className)
+                //     if (project.name == e.target.className) {
+                //         console.log(project.todo);
+                //     }
+                // });
+                // console.log (projects);
+            }
+        });
     });
 }
 
-projectsEl.forEach(element => {
-    element.addEventListener('click', (e) => {
-        if (e.target.className) {
 
-            // get specific project
-            projects.forEach((project) => {
-
-                renderTodos(e.target.className)
-                if (project.name == e.target.className) {
-                    console.log(project.todo);
-                }
-            });
-            console.log (projects);
-        }
+function getItemClick () {
+    console.log("hi")
+    todoItemsListEl.forEach(item => {
+        item.addEventListener("click", (e) => {
+            console.log(e.target.className);
+        })
     });
-});
+}
 
 
-console.log(today)
+// console.log(today)
 
-export { defaultSidebarEl_evL }
+export { defaultSidebarEl_evL, projectSidebarEl_evL, getItemClick }
